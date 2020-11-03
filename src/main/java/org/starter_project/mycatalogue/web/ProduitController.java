51d803cfd45302bb3096810487c3598ccafbc97e
hajah-dev <59752590+hajah-dev@users.noreply.github.com>
2020-11-02 23:30:21 -0500

    Update ProduitController.java

--- src/main/java/org/starter_project/mycatalogue/web/ProduitController.java
+++ src/main/java/org/starter_project/mycatalogue/web/ProduitController.java
@@ -38,34 +38,19 @@ public class ProduitController {
 
         model.addAttribute("currentPage",page1);
 
-        /**
-         * Pour garder le mot clé chercher dans la zone de text
-         */
+
         model.addAttribute("motCle", mc);
         return "products"; // products.html
     }
     @GetMapping("/delete")
     public String delete(Long id, int page, String motCle){
-        /**
-         * On peut ne pas mettre @RequestParam parce que par défaut, Spring
-         * cherche un paramètre qui s'appelle id.(Le paramètre porte le même nom que
-         * la variable)
-         */
+
         produitRepository.deleteById(id);
-        // Dans le code suivant on va demander à Spring de nous rediriger vers
-        // "/products" et la page courante où on a suprrimé un produit en gardant
-        // le mot clé dans le cas où il s'agirait d'une suppression après une recherche.
+
         return "redirect:/products?page="+page+"&motCle="+motCle;
     }
 
 
-    /**
-     * Validation:
-     * Pour procéder aux validations: quand on appel le formulaire, on a besoin d'y utiliser
-     * le Model (interface appartenant à Spring) comme argument. Une fois qu'on ait
-     * défini l'attribut qu'on va exploiter dans formproduit.html, on va y faire pour chaque
-     * champ "un data-binding"
-     */
     @GetMapping("/formProduit")
     public String form(Model model){
         model.addAttribute("produit", new Produit());
